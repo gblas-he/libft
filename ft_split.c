03de87528575f32574e262b8eebd34550b5fbdae
Giovanni Blas Heredia <gblas-he@c1r8s6.42madrid.com>
2026-01-25 19:34:28 +0100

    split1

--- ft_split.c
+++ ft_split.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/25 18:13:49 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/01/25 18:15:26 by gblas-he         ###   ########.fr       */
+/*   Created: 2026/01/25 18:33:33 by gblas-he          #+#    #+#             */
+/*   Updated: 2026/01/25 19:34:06 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+char **ft_split(char const *s, char c)
 {
-    
+    int i;
+    (void) c;
+    char ** cc = NULL;
+
+    i =0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    printf("%d", i);
+    return(cc);
+}
+
+int main (void)
+{
+    printf("%p", ft_split("Hello World", 32));
+    return 0;
 }
